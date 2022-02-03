e4e6b0d347a08adb90d8e3f3a98ad14d9dfada4f
kr <kr@xph.us>
2022-02-03 15:02:04 -0800

    elide and show types more carefully
    The rule of thumb is, when the type is statically known from
    context, elide the type. When it's not, show the type.
    
    In practice this means usually when the a value's context
    (struct field, slice element type, etc) has a static type
    that's an interface, we show the value's concrete type, and
    otherwise we don't.
    
    There are a few exceptions. For instance, when a type
    contains two or more adjacent levels of pointer indirection,
    e.g. []**int, we show the type to be more explicit.

--- format_test.go
+++ format_test.go
@@ -10,6 +10,12 @@ import (
 )
 
 func TestWriteShort(t *testing.T) {
+	type Bool bool
+	type Int int
+	type String string
+	type Empty struct{}
+	type Slice []int
+	type Chan chan int
 	type T struct{ V any }
 	cases := []struct {
 		v    any
@@ -21,33 +27,130 @@ func TestWriteShort(t *testing.T) {
 		{struct{ V int }{0}, "struct{ V int }{V:0}"},
 		{struct{ V, U int }{}, "struct{ V int; U int }{V:0, ...}"},
 		{struct{ V int }{0}, "struct{ V int }{V:0}"},
-		{(func())(nil), "nil"},
+		{(func())(nil), "(func())(nil)"},
 		{func() {}, "func() {...}"},
-		{(map[int]int)(nil), "nil"},
+		{map[int]int(nil), "map[int]int(nil)"},
 		{map[int]int{}, "map[int]int{}"},
 		{map[int]int{0: 0}, "map[int]int{0:0}"},
 		{map[int]int{0: 0, 1: 1}, "map[int]int{0:0, ...}"},
-		{(*int)(nil), "nil"},
-		{ptr(0), "&0"},
-		{ptr(ptr(0)), "&&0"},
-		{([]int)(nil), "nil"},
+		{(*int)(nil), "(*int)(nil)"},
+		{ptr(0), "&int(0)"},
+		{ptr(ptr(0)), "&&int(0)"},
+		{&T{V: 0}, "&diff.T{V:int(0)}"},
+		{[]int(nil), "[]int(nil)"},
 		{[]int{}, "[]int{}"},
 		{[]int{0}, "[]int{0}"},
 		{[]int{0, 1}, "[]int{0, ...}"},
 		{false, "false"},
-		{0, "0"},
+		{0, "int(0)"},
 		{"a", `"a"`},
-		{(chan int)(nil), "nil"},
+		{(chan int)(nil), "(chan int)(nil)"},
+		{Chan(nil), "diff.Chan(nil)"},
 		{make(chan int), "chan int"},
 		{unsafe.Pointer(new(int)), "unsafe.Pointer(...)"},
 		{unsafe.Pointer(uintptr(0)), "unsafe.Pointer(0)"},
+
+		// Truncate nested values.
 		{T{V: [1]int{0}}, "diff.T{V:[1]int{...}}"},
+		{[]any{[0]int{}}, "[]any{[0]int{}}"},
 		{T{V: T{V: 0}}, "diff.T{V:diff.T{...}}"},
 		{T{V: map[int]int{0: 0}}, "diff.T{V:map[int]int{...}}"},
+		{T{V: map[int]int{}}, "diff.T{V:map[int]int{}}"},
 		{T{V: []int{0}}, "diff.T{V:[]int{...}}"},
+		{T{V: []int{}}, "diff.T{V:[]int{}}"},
 		{[1]any{T{V: 0}}, "[1]any{diff.T{...}}"},
 		{map[int]any{0: T{V: 0}}, "map[int]any{0:diff.T{...}}"},
 		{[]any{T{V: 0}}, "[]any{diff.T{...}}"},
+		{[]any{Empty{}}, "[]any{diff.Empty{}}"},
+
+		// Elide concrete type when it's known from context.
+		//  * non-nilable containers
+		{[1][0]int{{}}, "[1][0]int{{}}"},
+		{[1]struct{}{{}}, "[1]struct{}{{}}"},
+		{[1]Empty{{}}, "[1]diff.Empty{{}}"},
+
+		// Elide concrete type when it's known from context.
+		//  * nilable types
+		{[1]func(){func() {}}, "[1]func(){func() {...}}"},
+		{[1]map[int]int{{}}, "[1]map[int]int{{}}"},
+		{[1]*int{ptr(1)}, "[1]*int{&1}"},
+		{[1]**int{ptr(ptr(1))}, "[1]**int{&&int(1)}"},                  // 2+ ptrs, don't elide
+		{[1]**int{ptr((*int)(nil))}, "[1]**int{&(*int)(nil)}"},         // 2+ ptrs, don't elide
+		{[1]**Empty{ptr(&Empty{})}, "[1]**diff.Empty{&&diff.Empty{}}"}, // 2+ ptrs, don't elide
+		{[1][]int{{}}, "[1][]int{{}}"},
+		{[1]Slice{{}}, "[1]diff.Slice{{}}"},
+		{[1]chan int{make(chan int)}, "[1]chan int{chan int}"},
+
+		// Elide concrete type when it's known from context.
+		//  * nil
+		{[1]func(){nil}, "[1]func(){nil}"},
+		{[1]map[int]int{nil}, "[1]map[int]int{nil}"},
+		{[1]*int{nil}, "[1]*int{nil}"},
+		{[1]**int{nil}, "[1]**int{nil}"},
+		{[1][]int{nil}, "[1][]int{nil}"},
+		{[1]chan int{nil}, "[1]chan int{nil}"},
+
+		// Elide concrete type when it's known from context.
+		//  * simple types
+		{[1]bool{false}, "[1]bool{false}"},
+		{[1]Bool{false}, "[1]diff.Bool{false}"},
+		{[1]int{0}, "[1]int{0}"},
+		{[1]Int{0}, "[1]diff.Int{0}"},
+		{[1]string{"a"}, `[1]string{"a"}`},
+		{[1]String{"a"}, `[1]diff.String{"a"}`},
+
+		// Show concrete type when it's not known from context.
+		//  * non-nilable containers
+		{[1]any{[0]int{}}, "[1]any{[0]int{}}"},
+		{[1]any{struct{}{}}, "[1]any{struct{}{}}"},
+		{[1]any{Empty{}}, "[1]any{diff.Empty{}}"},
+
+		// Show concrete type when it's not known from context.
+		//  * nilable types
+		{[1]any{func() {}}, "[1]any{func() {...}}"},
+		{[1]any{map[int]int{}}, "[1]any{map[int]int{}}"},
+		{[1]any{ptr(1)}, "[1]any{&int(1)}"},
+		{[1]any{ptr(ptr(1))}, "[1]any{&&int(1)}"},
+		{[1]any{ptr((*int)(nil))}, "[1]any{&(*int)(nil)}"},
+		{[1]any{[]int{}}, "[1]any{[]int{}}"},
+		{[1]any{Slice{}}, "[1]any{diff.Slice{}}"},
+		{[1]any{make(chan int)}, "[1]any{chan int}"},
+
+		// Show concrete type when it's not known from context.
+		//  * nil
+		{[1]any{(func())(nil)}, "[1]any{(func())(nil)}"},
+		{[1]any{map[int]int(nil)}, "[1]any{map[int]int(nil)}"},
+		{[1]any{(*int)(nil)}, "[1]any{(*int)(nil)}"},
+		{[1]any{(**int)(nil)}, "[1]any{(**int)(nil)}"},
+		{[1]any{[]int(nil)}, "[1]any{[]int(nil)}"},
+		{[1]any{(chan int)(nil)}, "[1]any{(chan int)(nil)}"},
+
+		// Show concrete type when it's not known from context.
+		//  * simple types
+		{[1]any{false}, "[1]any{false}"},
+		{[1]any{Bool(false)}, "[1]any{diff.Bool(false)}"},
+		{[1]any{0}, "[1]any{int(0)}"},
+		{[1]any{Int(0)}, "[1]any{diff.Int(0)}"},
+		{[1]any{"a"}, `[1]any{"a"}`},
+		{[1]any{String("a")}, `[1]any{diff.String("a")}`},
+
+		// Special case, omit "&" during elision inside arrays & slices.
+		{[1]*Empty{{}}, "[1]*diff.Empty{{}}"},
+		{[]*Empty{{}}, "[]*diff.Empty{{}}"},
+		{[1]any{&Empty{}}, "[1]any{&diff.Empty{}}"},
+		{[]any{&Empty{}}, "[]any{&diff.Empty{}}"},
+
+		// Trigger elision from each container.
+		{[1]int{0}, "[1]int{0}"},
+		{struct{ V int }{0}, "struct{ V int }{V:0}"},
+		{map[int]int{0: 0}, "map[int]int{0:0}"},
+		{[]int{0}, "[]int{0}"},
+
+		// Trigger non-elision from each container.
+		{[1]any{0}, "[1]any{int(0)}"},
+		{struct{ V any }{0}, "struct{ V any }{V:int(0)}"},
+		{map[int]any{0: 0}, "map[int]any{0:int(0)}"},
+		{[]any{0}, "[]any{int(0)}"},
 	}
 
 	for i, tt := range cases {
