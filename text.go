e4a5410f8a55dd15e6bf8aca1c71930324d729dc
kr <kr@xph.us>
2022-02-24 02:45:06 -0800

    format multiline text diffs ourselves
    This makes it easier to alter the formatting in the future,
    to show whitespace changes.

--- text.go
+++ text.go
@@ -3,12 +3,14 @@ package diff
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"strings"
 	"unicode/utf8"
 
-	"github.com/pkg/diff"
 	"github.com/pkg/diff/myers"
 )
+
+const nContext = 3
 
 func (d *differ) textDiff(e emitfer, a, b string) {
 	d.config.helper()
@@ -66,10 +68,74 @@ func textCheck(s, sep string, nmin, amax int) bool {
 type diffTextFormatter struct{ a, b, aLabel, bLabel string }
 
 func (df *diffTextFormatter) Format(f fmt.State, verb rune) {
-	err := diff.Text(df.aLabel, df.bLabel, df.a, df.b, f)
-	if err != nil {
-		panic(err)
+	fmt.Fprintf(f, "--- %s\n", df.aLabel)
+	fmt.Fprintf(f, "+++ %s\n", df.bLabel)
+	as := strings.Split(df.a, "\n")
+	bs := strings.Split(df.b, "\n")
+	ab := &slicePair[string]{a: as, b: bs}
+
+	merged := merge(myers.Diff(context.Background(), ab))
+
+	for i := 0; i < len(merged); {
+		ed := merged[i]
+		i1 := i + 1
+		for i1 < len(merged) && (aIsClose(merged, i1) || bIsClose(merged, i1)) {
+			i1++
+		}
+		ed1 := merged[i1-1]
+
+		a0, b0 := 0, 0
+		a1, b1 := len(as), len(bs)
+		if n := ed.a0 - nContext; n > 0 {
+			a0 = n
+		}
+		if n := ed.b0 - nContext; n > 0 {
+			b0 = n
+		}
+		if n := ed1.a1 + nContext; n < a1 {
+			a1 = n
+		}
+		if n := ed1.b1 + nContext; n < b1 {
+			b1 = n
+		}
+
+		fmt.Fprintf(f, "@@ -%s +%s @@\n",
+			lineRange(a0, a1-a0),
+			lineRange(b0, b1-b0),
+		)
+		for a0 < a1 || b0 < b1 {
+			if a0 < ed.a0 || i > i1 {
+				fmt.Fprintf(f, " %s\n", as[a0])
+				a0++
+				b0++
+			} else if a0 < ed.a1 {
+				fmt.Fprintf(f, "-%s\n", as[a0])
+				a0++
+			} else if b0 < ed.b1 {
+				fmt.Fprintf(f, "+%s\n", bs[b0])
+				b0++
+			}
+			if a0 >= ed.a1 && b0 >= ed.b1 {
+				i++
+				if i < len(merged) {
+					ed = merged[i]
+				}
+			}
+		}
 	}
+}
+
+func aIsClose(e []edit, i int) bool { return e[i].a0-e[i-1].a1 <= 2*nContext }
+func bIsClose(e []edit, i int) bool { return e[i].b0-e[i-1].b1 <= 2*nContext }
+
+func lineRange(r0, r1 int) string {
+	switch r1 - r0 {
+	case 0:
+		return fmt.Sprintf("%d,0", r0)
+	case 1:
+		return strconv.Itoa(r0)
+	}
+	return fmt.Sprintf("%d,%d", r0+1, r1-r0)
 }
 
 type slicePair[T comparable] struct{ a, b []T }
